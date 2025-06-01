5ca9d9f82955b44eb5e6b43b5a1c88744d72790b
anandiooooooooooooooooooooooooooooooooo <anandhio123@gmail.com>
2025-06-02 01:56:09 +0700

    ai ai an

--- assets/js/siswa-classes-1.js
+++
@@ -1,4 +0,0 @@
-function selectClass(subjectName) {
-    localStorage.setItem("selectedClass", subjectName);
-    window.location.href = "./siswa-classes-2.html";
-}
--- assets/js/siswa-classes-2.js
+++
@@ -1,4 +0,0 @@
-const className = localStorage.getItem("selectedClass");
-if (className) {
-    document.getElementById("class-name").textContent = className;
-}
